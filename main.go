c2a34be234ec4854969ec28f7bba8712c3e36f71
bonefabric <iburanguloff@ya.ru>
2022-11-04 05:13:40 +0300

    untar func & fixes

--- main.go
+++ main.go
@@ -11,20 +11,17 @@ import (
 
 var fileFlag string
 var extractFlag bool
+var pathFlag string
 
 func init() {
 	flag.StringVar(&fileFlag, "f", "out.tar", "tar file name")
 	flag.BoolVar(&extractFlag, "x", false, "extract tar")
+	flag.StringVar(&pathFlag, "p", ".", "extract path")
 
 	flag.Parse()
 }
 
 func main() {
-	if len(flag.Args()) == 0 {
-		println("files required")
-		os.Exit(1)
-	}
-
 	if extractFlag {
 		if err := toUntar(); err != nil {
 			println("failed to extract tar file")
@@ -40,6 +37,11 @@ func main() {
 }
 
 func toTar() error {
+	if len(flag.Args()) == 0 {
+		println("files required")
+		os.Exit(1)
+	}
+
 	tarAbs, err := filepath.Abs(fileFlag)
 	if err != nil {
 		return err
@@ -69,27 +71,16 @@ func toTar() error {
 	}
 
 	walkFunc := func(path string, d fs.DirEntry, err error) error {
-		if d.IsDir() || path == tarAbs {
+		if path == tarAbs {
 			return nil
 		}
 
-		relPath, err := filepath.Rel(curAbs, path)
-		if err != nil {
-			return err
-		}
-
-		src, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-
-		defer func(src *os.File) {
-			if err := src.Close(); err != nil {
-				println("failed to close file " + src.Name())
-			}
-		}(src)
-
 		finfo, err := d.Info()
+		if err != nil {
+			return err
+		}
+
+		relPath, err := filepath.Rel(curAbs, path)
 		if err != nil {
 			return err
 		}
@@ -104,6 +95,21 @@ func toTar() error {
 		if err := tw.WriteHeader(hdr); err != nil {
 			return err
 		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		src, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+
+		defer func(src *os.File) {
+			if err := src.Close(); err != nil {
+				println("failed to close file " + src.Name())
+			}
+		}(src)
 
 		if _, err := io.Copy(tw, src); err != nil {
 			return err
@@ -127,6 +133,56 @@ func toTar() error {
 }
 
 func toUntar() error {
-	//todo realuze
+	absExPath, err := filepath.Abs(pathFlag)
+	if err != nil {
+		return err
+	}
+
+	absTarPath, err := filepath.Abs(fileFlag)
+	if err != nil {
+		return err
+	}
+
+	tarFile, err := os.Open(absTarPath)
+	if err != nil {
+		return err
+	}
+
+	defer func(tarFile *os.File) {
+		if err := tarFile.Close(); err != nil {
+			println("failed to close tar file")
+		}
+	}(tarFile)
+
+	tr := tar.NewReader(tarFile)
+
+	for {
+		hdr, err := tr.Next()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+
+		absTgtPath := filepath.Join(absExPath, hdr.Name)
+
+		if hdr.FileInfo().IsDir() {
+			if err := os.MkdirAll(absTgtPath, hdr.FileInfo().Mode()); err != nil {
+				return err
+			}
+			continue
+		}
+
+		tgtFile, err := os.OpenFile(absTgtPath, os.O_CREATE|os.O_WRONLY, hdr.FileInfo().Mode())
+
+		if _, err := io.Copy(tgtFile, tr); err != nil {
+			return err
+		}
+
+		if err := tgtFile.Close(); err != nil {
+			return err
+		}
+	}
 	return nil
 }
